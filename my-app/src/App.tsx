340d17f75b7c7bd83f58b8289f8f0744becf418b
NickRM22 <nick.rodrigues2005@gmail.com>
2026-08-13 23:13:47 -0300

    feat: Integrando Componentes com modularização

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,15 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
 
 
 export default function App(){
+  
   return(
     <div>
-      <h1>Meu Componente</h1>
+      <Cabecalho></Cabecalho>
+      <Conteudo></Conteudo>
+      <Rodape></Rodape>
     </div>
   )
 }
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+
+export default function Rodape(){
+    return(
+        <footer>
+            <p>Todos direitos reservados &copy;</p>
+        </footer>
+    )
+}
