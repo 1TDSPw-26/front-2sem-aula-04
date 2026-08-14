e9e25df61b01c0b9d90f1d04740c5e2cab45e409
murilloslopes <murillosivierolopes2310@gmail.com>
2026-08-13 23:13:55 -0300

    feat: Integrando Componentes com modularização.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <h2>Conteúdo Principal</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, nobis adipisci. Similique, dolorum soluta sunt ratione maiores rem suscipit. Dolor officia nemo, eveniet ex natus nostrum sequi quisquam dolores harum.</p>
+    </main>
+  );
+}
