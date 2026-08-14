761807577ce623a24f136fb4e3fa8b787bff3124
pedrodaniel7 <02bouth@gmail.com>
2026-08-13 23:13:22 -0300

    feat: integrando componentes com modularização

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,9 @@
+
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo aleatório</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quibusdam ut officiis mollitia blanditiis sint dolores itaque perspiciatis quasi, dolor recusandae reiciendis, est impedit magni illo, id non sapiente eligendi?</p>
+        </main>
+    );
+}
