a35f81a2b141e01177b470441be86c449db15a5f
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-08-13 23:14:18 -0300

    feat: Integrando Componentes com Modularização.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,12 @@
+export default function Conteudo() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sint vel
+        quis numquam dolorum debitis quaerat, illum autem aspernatur laudantium
+        accusantium, voluptas perspiciatis? Temporibus impedit, praesentium
+        sapiente nesciunt pariatur ratione.
+      </p>
+    </main>
+  );
+}
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape() {
+    return(
+        <footer>
+            <p>&copy; 2026 - Footer</p>
+        </footer>
+    )
+}
