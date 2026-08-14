f4ed3135043b91ab8f9020c541baf5517fe5e383
proc0x41 <20628001+proc0x41@users.noreply.github.com>
2026-08-13 23:48:57 -0300

    feat: Integrando componentes com modularização

---
+++ my-project/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo aleatório</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae nemo culpa eos numquam quisquam excepturi voluptatum dolore nisi. Tempore, nisi tenetur blanditiis ullam doloribus repellendus culpa facilis esse aliquam asperiores?</p> 
+        </main>
+    );
+}
