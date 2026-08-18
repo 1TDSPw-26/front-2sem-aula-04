3c48a4c9d18a0151d7f01d8f02e4a2c96231f836
andreisou-dev <andreisousa250208@gmail.com>
2026-08-18 05:47:53 -0300

    feat: Integrando Componentes com modularização

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,10 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo aleatório</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi molestiae saepe id voluptates omnis.
+                 Exercitationem nobis officia numquam, expedita quibusdam reiciendis ex suscipit et tempora, quam,
+                 voluptas voluptatum incidunt? Vitae!</p>
+        </main>
+    );
+}
